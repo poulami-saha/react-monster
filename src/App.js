198b336b942d76235787e7d9b866164e857d399f
Poulami Saha <saha.poulami91@gmail.com>
2020-05-24 21:29:37 +0530

    A very basic react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { CardList } from "./components/card-lists/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchText: "",
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState({ monsters: users });
+      });
+  }
+  handleChange = (e) => this.setState({ searchText: e.target.value });
+
+  render() {
+    const { monsters, searchText } = this.state;
+    const filteredmonsters = monsters.filter((mons) =>
+      mons.name.toLocaleLowerCase().includes(searchText.toLocaleLowerCase())
+    );
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="Search Monster"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredmonsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/card/card.component.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./card.styles.css";
+export const Card = (props) => {
+  return (
+    <div className="card-container">
+      <img
+        src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+        alt="monsters"
+      />
+      {props.monster.name}
+      <p>{props.monster.email}</p>
+    </div>
+  );
+};
